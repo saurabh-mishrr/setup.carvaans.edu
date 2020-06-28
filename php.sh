46f2293c3fa65e4a539c85c004093ce2dd37c684
Saurabh Mishra <saurabhmishra@Saurabhs-MacBook-Air.local>
2020-06-28 21:28:48 +0530

    intial setup

--- php.sh
+++ php.sh
@@ -12,6 +12,7 @@ setup_php() {
             -p '${PHP_PORT}' 
             -v '${PROJECT_DIR}'/api:/app:rw 
             -v '${HOME}'/.ssh:/root/.ssh:ro
+            -v '${PHP_INI}':/usr/local/etc/php/conf.d/php.ini
             -w /app 
             --hostname '${PHP_CONTAINER_NAME}' 
             --name '${PHP_CONTAINER_NAME}' 
